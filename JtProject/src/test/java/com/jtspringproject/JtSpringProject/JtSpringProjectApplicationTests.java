9ce54d3e77caa3b4149ad1ffd5cdbe2a97cffdcc
priyansh kakani <priyanshkakani13@gmail.com>
2025-07-12 22:05:41 +0530

    temporary changes made priyansh!

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -3,10 +3,8 @@ package com.jtspringproject.JtSpringProject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JtSpringProjectApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
